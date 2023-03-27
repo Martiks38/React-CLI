3de9c23a730c842991cc4b6bab84964361152742
Martiks38 <cappa.giuliano@gmail.com>
2023-03-26 21:40:41 -0300

    Add root command

--- src/commands.js
+++ src/commands.js
@@ -1,4 +1,6 @@
+import { readFileSync, writeFileSync } from 'node:fs'
 import process from 'node:process'
+import pc from 'picocolors'
 import { generateComponent } from './generate/generateComponent.js'
 import { generateHook } from './generate/generateHook.js'
 
@@ -30,6 +32,33 @@ export function generateCommands({ args, cliConfigFile, program }) {
 			} catch (error) {
 				console.error(`[${error.name}]: ${error.message}`)
 				process.exitCode = 1
+			}
+		})
+
+	program
+		.command('root')
+		.description('Shows the root where the files will be generated')
+		.option('--change <changeValue>', 'Change the baseURL value of the configuration file')
+		.action((options) => {
+			const configFile = readFileSync('./rcliconfig.json', 'utf-8')
+			const config = JSON.parse(configFile)
+
+			if (options.change === undefined) {
+				console.log(`\n${pc.blue('Root')}: ${config.baseURL}`)
+				return
+			}
+
+			let newConfig = JSON.stringify({ ...config, baseURL: options.change }, null, 2)
+
+			try {
+				writeFileSync('./rcliconfig.json', newConfig)
+
+				console.log(`\n${pc.green('SUCESS')} Configuration file modified successfully`)
+			} catch (err) {
+				const error = new Error('Error modifying configuration file')
+
+				error.name = 'ConfigError'
+				throw error
 			}
 		})
 
