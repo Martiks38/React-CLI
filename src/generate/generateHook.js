382ef271ce0c86a9b88625a87d38a826eb69e48e
Martiks38 <cappa.giuliano@gmail.com>
2023-03-29 00:54:13 -0300

    Modify generateHook

--- src/generate/generateHook.js
+++ src/generate/generateHook.js
@@ -1,16 +1,20 @@
-import { existsSync, mkdirSync, writeFile } from 'node:fs'
+import { existsSync, mkdirSync, writeFileSync } from 'node:fs'
 import path from 'node:path'
 import pc from 'picocolors'
 import tempHook from '../template/hook.js'
 
-export function generateHook({ cliConfigFile, name }) {
-	const { baseURL, usesTypeScript } = cliConfigFile
-	const fileExtension = usesTypeScript ? '.ts' : '.js'
-	const template = tempHook.replace('useHook', name)
-
-	const route = path.join(baseURL, 'hooks')
-
-	if (existsSync(path.join(route, name + fileExtension))) {
+/**
+ * @description Check if the hook already exists and if it meets the naming convention.
+ * @param { Object } dataHook - Information of the hook.
+ * @param { string } dataHook.name - The name of the hook.
+ * @param { string } dataHook.pathHook - The path of the hook.
+ *
+ * @throws Throws an error if the file already exists or does not comply with the hook naming convention.
+ * @throws { HookExistsError } -The hook must not exist.
+ * @throws { HookNameError } - The hook must comply with the convention.
+ * */
+function checkHook({ name, pathHook }) {
+	if (existsSync(pathHook)) {
 		const error = new Error(`The ${name} hook is already created`)
 
 		error.name = 'HookExistsError'
@@ -24,13 +28,39 @@ export function generateHook({ cliConfigFile, name }) {
 		throw error
 	}
 
-	if (name.length === 3) {
-		const error = new Error('Invalid name. A valid name, for example, is username.')
+	let firstLetter = name.split('use')[1].charAt(0)
+	let isNotPascalCase = /[a-z]/.test(firstLetter)
+
+	if (isNotPascalCase) {
+		const error = new Error('Hooks begin with "use" followed by an uppercase letter.')
 
 		error.name = 'HookNameError'
 		throw error
 	}
+}
 
+/**
+ *
+ * @param { Object }
+ * @param { Object }
+ * @param { string }
+ * @throws Throws an error if it fails to create hook file.
+ * @throws { InternalError }
+ */
+export function generateHook({ cliConfigFile, name }) {
+	const { baseURL, usesTypeScript } = cliConfigFile
+
+	const fileExtension = usesTypeScript ? '.ts' : '.js'
+	const nameFile = name.concat(fileExtension)
+
+	const route = path.join(baseURL, 'hooks')
+	const pathHook = path.join(baseURL, 'hooks', nameFile)
+
+	checkHook({ name, pathHook })
+
+	const fileContent = tempHook.replace('useHook', name)
+
+	// Generate the folders if they don't exist
 	route.split(path.sep).reduce((prevPath, folder) => {
 		const currentPath = path.join(prevPath, folder)
 		if (!existsSync(currentPath)) mkdirSync(currentPath)
@@ -38,14 +68,14 @@ export function generateHook({ cliConfigFile, name }) {
 		return currentPath
 	}, '')
 
-	writeFile(path.join(route, name + fileExtension), template, (error) => {
-		if (error) {
-			const err = new Error('An error occurred while generating the hook')
+	try {
+		writeFileSync(pathHook, fileContent)
 
-			err.name = 'InternalError'
-			throw err
-		}
-	})
+		console.log(pc.green(`\n${name} created successfully`))
+	} catch (error) {
+		const err = new Error('An error occurred while generating the hook')
 
-	console.log(pc.green(`${name} created successfully`))
+		err.name = 'InternalError'
+		throw err
+	}
 }
